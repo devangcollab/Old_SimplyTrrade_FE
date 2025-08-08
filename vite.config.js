68f88a30d169d239326776d90acd9542a3ac74c8
devangpatel <devang.patel@collabsoftech.com.au>
2025-08-08 11:25:30 +0530

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
